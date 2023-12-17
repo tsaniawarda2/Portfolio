7fa8a40ff06495df7c308c94d64c1eda6296a0ca
tsaniawarda2 <63621268+tsaniawarda2@users.noreply.github.com>
2023-12-18 03:00:39 +0700

    home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./home.css";
 import Social from "./Social";
 import Data from "./Data";
+import ScrollDown from "./ScrollDown";
 
 const Home = () => {
   return (
@@ -15,6 +16,8 @@ const Home = () => {
 
             <Data />
           </div>
+
+          <ScrollDown />
         </div>
       </section>
     </>
