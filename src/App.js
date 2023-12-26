7a2914605414d84fe33dd170ce48fd676ed63e40
tsaniawarda2 <63621268+tsaniawarda2@users.noreply.github.com>
2023-12-27 01:03:41 +0700

    portfolio done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Qualification from "./components/qualification/Qualification";
+import Work from "./components/work/Work";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <About />
         <Skills />
         <Qualification />
+        <Work />
       </main>
     </>
   );
---
+++ src/components/work/Data.jsx
@@ -0,0 +1,58 @@
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/work5.jpg";
+
+export const projectsData = [
+  {
+    id: 1,
+    image: Work1,
+    title: "Web Development",
+    category: "web",
+    link: "https://github.com/tsaniawarda2",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "App Mobile",
+    category: "app",
+    link: "",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "Brand Design",
+    category: "design",
+    link: "",
+  },
+  {
+    id: 4,
+    image: Work4,
+    title: "Web Development",
+    category: "web",
+    link: "",
+  },
+  {
+    id: 5,
+    image: Work5,
+    title: "Web Development",
+    category: "web",
+    link: "",
+  },
+];
+
+export const projectsNav = [
+  {
+    name: "all",
+  },
+  {
+    name: "web",
+  },
+  {
+    name: "app",
+  },
+  {
+    name: "design",
+  },
+];
